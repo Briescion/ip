b76b654ebcfec60e968cbb58074877a0ae3710e1
briescion <benjaminseahwz@gmail.com>
2025-08-21 18:19:34 +0800

    increment 1

--- src/main/java/Dan.java
+++ src/main/java/Dan.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 public class Dan {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
+        String divider = "____________________________________________________________ ";
+
+        System.out.println("Hello! I'm Dan\n");
+        System.out.println("What can I do for you?\n");
+        System.out.println(divider);
+
+        String input = "";
+        while(!input.equals( "bye")) {
+            input = scanner.nextLine();
+            System.out.println(divider);
+        }
+
+        System.out.println("Bye. Hope to see you again soon!\n");
+        System.out.println(divider);
     }
 }
